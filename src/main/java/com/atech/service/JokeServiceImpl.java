0131f578cd488496bd25972004f150f621105068
raedgeorge <raedgeorge2014@gmail.com>
2022-01-26 23:11:29 +0200

    minor modifications, nothing primary

--- src/main/java/com/atech/service/JokeServiceImpl.java
+++ src/main/java/com/atech/service/JokeServiceImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class JokeServiceImpl implements JokeService{
 
-    private ChuckNorrisQuotes chuckNorrisQuotes;
+    private final ChuckNorrisQuotes chuckNorrisQuotes;
 
     @Autowired
     public JokeServiceImpl(ChuckNorrisQuotes chuckNorrisQuotes) {
